6a10fb8cf869d7bb02ac24fbb8c17045dcf85d31
AlenaKapkina <52639037+AlenaKapkina@users.noreply.github.com>
2019-07-31 15:44:23 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -8,46 +8,37 @@ public class Main {
         double x, y, d, f;
         if (a == 0 && b == 0 && c != 0) {
             System.out.println("Not an equation");
-        }
-        else if (a == 0 && b != 0 && c == 0) {
+        } else if (a == 0 && b != 0 && c == 0) {
             x = 0;
             System.out.println(x);
-        }
-        else if (a == 0 && b != 0 && c != 0) {
-            x = (-c)/b;
+        } else if (a == 0 && b != 0 && c != 0) {
+            x = (-c) / b;
             System.out.println(x);
-        }
-        else if (a != 0 && b == 0 && c != 0) {
-            f = (-c)/a;
+        } else if (a != 0 && b == 0 && c != 0) {
+            f = (-c) / a;
             if (f < 0) {
                 System.out.println("No real roots");
-            }
-            else {
+            } else {
                 x = Math.sqrt(f);
                 System.out.println(x);
             }
-        }
-        else if (a != 0 && b == 0 && c == 0) {
+        } else if (a != 0 && b == 0 && c == 0) {
             x = 0;
             System.out.println(x);
-        }
-        else if (a != 0 && b != 0 && c == 0) {
+        } else if (a != 0 && b != 0 && c == 0) {
             x = 0;
-            y = (-b)/a;
+            y = (-b) / a;
             System.out.println(x + " " + y);
-        }
-        else {
+        } else {
             d = Math.pow(b, 2) - 4 * a * c;
             if (d < 0) {
                 System.out.println("No real roots");
-            }
-            else if (d == 0) {
-                x = (-b)/(2 * a);
+            } else if (d == 0) {
+                x = (-b) / (2 * a);
                 System.out.println(x);
-            }
-            else {
-                x = (-b + Math.sqrt(d))/(2 * a);
-                y = (-b - Math.sqrt(d))/(2 * a);
+            } else {
+                x = (-b + Math.sqrt(d)) / (2 * a);
+                y = (-b - Math.sqrt(d)) / (2 * a);
                 System.out.println(x + " " + y);
             }
         }
